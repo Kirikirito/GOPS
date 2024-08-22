3c292ef6f6eb07dded642d9484d777ac474eb0a8
wwx <17888819590@163.com>
2024-08-22 14:57:07 +0800

    add test cases

--- idsim_run/run_cross_test_case.py
+++ idsim_run/run_cross_test_case.py
@@ -7,12 +7,13 @@ test_conf.render_only = False
 test_conf.generate_animation = True
 test_conf.sur_bias_range = [1.0, 1.5]
 test_conf.sur_bias_prob = 0.5
-test_conf.ini_network_root = r"/root/gops/results/idsim/idsim_multilane_exp_0813_2_new_attn/idsim_multilane_vec/dsact_pi/12345_2000000_run0"
-test_conf.nn_index = '950000'
+test_conf.ini_network_root = r"/root/gops/results/idsim/idsim_cross_exp_0811/idsim_cross_vec/dsact_pismonet/12345_2000000_run0"
+test_conf.nn_index = '1950000'
 test_conf.dpi = 60
 test_conf.test_name = 'withsur'
-test_configs_file = '/root/gops/idsim_run/test_configs/multilane_withsur.json'
+test_configs_file = '/root/gops/idsim_run/test_configs/cross_withsur.json'
 
 
 if __name__ == "__main__":
     parallel_eval(test_conf, test_configs_file)
+
