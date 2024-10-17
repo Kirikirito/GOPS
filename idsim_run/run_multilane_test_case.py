a1eab7af3851cc015b6f68da7a9bc8d21e52d37c
wwx <17888819590@163.com>
2024-10-17 15:06:18 +0800

    update sctipts

--- idsim_run/run_multilane_test_case.py
+++ idsim_run/run_multilane_test_case.py
@@ -3,14 +3,14 @@ from utils import parallel_eval
 from easydict import EasyDict as edict
 
 test_conf= edict()
-test_conf.render_only = False
+test_conf.render_only = True
 test_conf.generate_animation = True
-test_conf.sur_bias_range = [1.0, 1.5]
+test_conf.sur_bias_range = [0.3, 0.5]
 test_conf.sur_bias_prob = 0.5
 test_conf.ini_network_root = r"/root/gops/results/idsim/idsim_multilane_exp_0826/idsim_multilane_vec/dsact_pi/12345_2000000_run0"
 test_conf.nn_index = '1500000'
 test_conf.dpi = 60
-test_conf.test_name = 'withsur_final'
+test_conf.test_name = 'withsur_final_test'
 test_configs_file = '/root/gops/idsim_run/test_configs/multilane_withsur.json'
 
 
